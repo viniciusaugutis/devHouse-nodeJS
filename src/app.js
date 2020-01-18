35e02cf1199847a8eb112cfd2bd0212956956391
Vinícius Augutis <vinicius_augutis@hotmail.com>
2020-01-18 10:47:41 -0300

    Mongoose e conectando no banco NoSQL

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 //Todas configurações do Express
 
 import express from 'express';
+import mongoose from 'mongoose';
+
 import routes from './routes';
 
 class App {
   constructor() {
     this.server = express();
+
+    mongoose.connect('mongodb+srv://devhouse:devhouse@devhouse-ve9at.mongodb.net/devhouse?retryWrites=true&w=majority', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    
     this.middlewares();
     this.routes();
   }
