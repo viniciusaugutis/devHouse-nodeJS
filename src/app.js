4ebbccfe230c3a767d53c47af59325b590c7383c
Vinícius Augutis <vinicius_augutis@hotmail.com>
2020-01-18 12:15:38 -0300

    Adicionando CORS na API

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
 
 import express from 'express';
 import mongoose from 'mongoose';
+import cors from 'cors';
 import path from 'path';
 
 import routes from './routes';
@@ -20,7 +21,7 @@ class App {
   }
 
   middlewares() {
-
+    this.server.use(cors());
     this.server.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
     this.server.use(express.json());
   }
